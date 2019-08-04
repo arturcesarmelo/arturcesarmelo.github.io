2534525610b2f5b08cd194052f396a2639737580
Tutti Cesar <arturcesarmelo@hotmail.com>
2019-08-04 11:09:02 -0300

    mudanca nos graficos

--- dist/js/app.js
+++ dist/js/app.js
@@ -17,30 +17,24 @@ app.config(function($stateProvider, $urlRouterProvider) {
 });
 app.controller('chartCtrl', function($scope) {
   $scope.labels = [
-      'JavaScript',
-      'PHP',
-      'Ruby',
       'Laravel',
-      'Cake',
-      'Symfony',
-      'Rails',
+      'NodeJS',
+      'C#',
       'Angular',
-      'VueJS'
+      'VueJS',
+      'React'
     ];
   
     $scope.series = ['Percent of knowledge'];
 
     $scope.data = [
         [
-          87.5, // JavaScript
-          90, // PHP
-          75, // Ruby
           90, // Laravel
-          74, // Cake
-          75.5, // Symfony
-          75, // Rails
-          87.5, // Angular
-          87.5  // VueJS
+          75, // NodeJS
+          75, // C#
+          85,  // Angular
+          90,  // VueJS
+          75  // React
         ]
     ];
 })
